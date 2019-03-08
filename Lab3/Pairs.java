ca0a4a7e5d8fd0d40d7703c0654644585dc1328b
Anna S <31012236+iamannkat@users.noreply.github.com>
2019-03-08 21:10:01 +0200

    Create Pairs.java

---
+++ Lab3/Pairs.java
@@ -0,0 +1,15 @@
+public class Pair {    
+    private Character c; // character    
+    private int p; // position
+        
+    Pair(Character c, int p)
+    {
+        this.c = c; this.p = p;
+    }
+    
+    public int getP(){ 
+		return this.p; }
+    
+	public Character getC(){ 
+		return this.c; }
+}
